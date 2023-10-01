1641b700d6fd4d0d76c9084b02f2cdc7bc795c57
guyettinger <guy@pointivo.com>
2023-10-01 11:40:02 -0400

    Adding Editable Story

--- src/components/Editable/Editable.stories.tsx
+++ src/components/Editable/Editable.stories.tsx
@@ -1,4 +1,5 @@
 import { Meta, StoryObj } from "@storybook/react";
+import { useArgs } from "@storybook/preview-api";
 import Editable from "./Editable";
 import { EditableTypes } from "./Editable.types";
 
@@ -12,8 +13,19 @@ export default meta;
 type Story = StoryObj<typeof Editable>;
 
 export const Primary: Story = (args:any) => {
+    const [{value},updateArgs] = useArgs()
+
+    const onValueChange = (newValue: string) => {
+        // Call the provided callback
+        // This is used for the Actions tab
+        args.onValueChange?.(newValue);
+
+        // Update the arg in Storybook
+        updateArgs({ value: newValue });
+    };
+
     return (
-        <Editable data-testId="Editable-id" {...args} />
+        <Editable data-testId="Editable-id" {...args} value={value} onValueChanged={onValueChange} />
     )
 }
 Primary.args = {
