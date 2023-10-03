b4103f0836b28ec21f52d1a367c9d13e3e996daa
guyettinger <guy@pointivo.com>
2023-10-03 00:23:44 -0400

    Adding Representations

--- src/components/JsonView/JsonNodeView.tsx
+++ src/components/JsonView/JsonNodeView.tsx
@@ -1,9 +1,21 @@
-import {
-    JsonNodeViewProps
-} from "./JsonView.types";
+import styled from "styled-components";
+import { JsonNodeViewProps } from "./JsonView.types";
 import JsonNodeTitleView from "./JsonNodeTitleView";
 import JsonNodeValueView from "./JsonNodeValueView"
 import { useJsonViewApiContext } from "./JsonViewContext";
+
+const JsonNodeContent = styled.li`
+    display: flex;
+`
+
+const JsonNodeTitleContainer = styled.div`
+  flex-basis: fit-content;
+  padding-right: 4px;
+`
+
+const JsonNodeValueContainer = styled.div`
+    flex: 1;
+`
 
 const JsonNodeView = ({title, path, value}: JsonNodeViewProps) => {
     const api = useJsonViewApiContext()
@@ -14,10 +26,16 @@ const JsonNodeView = ({title, path, value}: JsonNodeViewProps) => {
     }
 
     return (
-        <li onClick={handleClick}>
-            {title && <JsonNodeTitleView title={title}/>}
-            <JsonNodeValueView path={path} value={value}/>
-        </li>
+        <JsonNodeContent onClick={handleClick}>
+            {title &&
+                <JsonNodeTitleContainer>
+                    <JsonNodeTitleView title={title}/>
+                </JsonNodeTitleContainer>
+            }
+            <JsonNodeValueContainer>
+                <JsonNodeValueView path={path} value={value}/>
+            </JsonNodeValueContainer>
+        </JsonNodeContent>
     )
 }
 export default JsonNodeView;
