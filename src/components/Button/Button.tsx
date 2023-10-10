00022e4603a56ac0f6c9b07b567823b1d9e88fa1
guyettinger <guy@pointivo.com>
2023-10-10 19:53:18 -0400

    Button Sizes

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -9,17 +9,22 @@ const StyledButton = styled.button<{
 }>`
   border: 0;
   line-height: 1;
-  font-size: 15px;
+  font-size: ${(props) =>
+          props.$variant === "small"
+                  ? "12px"
+                  : props.$variant === "medium"
+                          ? "14x"
+                          : "15px"};
   cursor: pointer;
   font-weight: bold;
   border-radius: 3px;
   display: inline-block;
   padding: ${(props) =>
           props.$variant === "small"
-                  ? "7px 25px 8px"
+                  ? "5px 20px 5px"
                   : props.$variant === "medium"
-                          ? "9px 30px 11px"
-                          : "14px 30px 16px"};
+                          ? "7px 25px 8px"
+                          : "9px 30px 11px"};
   color: ${(props) => (props.$primary ? props.theme.gle.color.buttonPrimaryForeground : props.theme.gle.color.buttonForeground)};
   background: ${(props) => (props.$primary ? props.theme.gle.color.buttonPrimaryBackground : props.theme.gle.color.buttonBackground)};
   opacity: ${(props) => (props.disabled ? 0.5 : 1)};
@@ -32,10 +37,10 @@ const StyledButton = styled.button<{
     border: solid 2px ${(props) => (props.$primary ? props.theme.gle.color.buttonPrimaryActiveBorderColor : props.theme.gle.color.buttonActiveBorderColor)};
     padding: ${(props) =>
             props.$variant === "small"
-                    ? "5px 23px 6px"
+                    ? "3px 18px 3px"
                     : props.$variant === "medium"
-                            ? "7px 28px 9px"
-                            : "12px 28px 14px"};
+                            ? "5px 23px 6px"
+                            : "7px 28px 9px"};
   }
 `
 
